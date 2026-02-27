8901a87e22aebbfb27a7e557125a8a40b8d4e174
Preben Vangberg <prv21fgt@bangor.ac.uk>
2026-02-27 12:16:40 +0000

    Replaced list typing with typing.List
    This is to ensure that we are compatible with Python 3.8

--- python-src/evaluatio/effect_size/cohen.py
+++ python-src/evaluatio/effect_size/cohen.py
@@ -1,7 +1,9 @@
+from typing import List
+
 import evaluatio._bindings as _bindings
 
 
-def cohens_d_independent(x1: list[float], x2: list[float]) -> float:
+def cohens_d_independent(x1: List[float], x2: List[float]) -> float:
     """Calculates Cohen's d for two independent samples.
 
     NOTE: At least two samples is required in each array for the variances to make sense.
@@ -11,10 +13,10 @@ def cohens_d_independent(x1: list[float], x2: list[float]) -> float:
     return _bindings.cohens_d_independent(x1, x2)
 
 
-def cohens_d_paired(x1: list[float], x2: list[float]) -> float:
+def cohens_d_paired(x1: List[float], x2: List[float]) -> float:
     """Calculates Cohen's d for two paired samples.
 
-    Should be corrected using the formula d'/sqrt(1-r) after computation. 
+    Should be corrected using the formula d'/sqrt(1-r) after computation.
 
     NOTE: At least two samples is required in each array for the variances to make sense.
 
