f15f11019f9df23d27803c7e4578132b449a0cff
Preben Vangberg <prv21fgt@bangor.ac.uk>
2026-09-22 13:31:08 +0100

    Standardised the remaining docstrings
    Went through and checked that the format for all functions
    looks ok. They might still need improvement, but at least
    the format should be fine.
    
    Resolved: #4

--- python-src/evaluatio/effect_size/cohen.py
+++ python-src/evaluatio/effect_size/cohen.py
@@ -6,9 +6,10 @@ import evaluatio._bindings as _bindings
 def cohens_d_independent(x1: Iterable[float], x2: Iterable[float]) -> float:
     """Calculates Cohen's d for two independent samples.
 
-    NOTE: At least two samples is required in each array for the variances to make sense.
-
-    NOTE: Should work with any type that is castable to a f64, hence ints, floats, and bools.
+    Notes
+    -----
+    At least two samples is required in each array for the variances to make sense.
+    Should work with any type that is castable to a f64, hence ints, floats, and bools.
     """
     return _bindings.cohens_d_independent(x1, x2)
 
@@ -16,8 +17,9 @@ def cohens_d_independent(x1: Iterable[float], x2: Iterable[float]) -> float:
 def cohens_d_paired(x1: Iterable[float], x2: Iterable[float]) -> float:
     """Calculates Cohen's d for two paired samples.
 
-    NOTE: At least two samples is required in each array for the variances to make sense.
-
-    NOTE: Should work with any type that is castable to a f64, hence ints, floats, and bools.
+    Notes
+    -----
+    At least two samples is required in each array for the variances to make sense.
+    Should work with any type that is castable to a f64, hence ints, floats, and bools.
     """
     return _bindings.cohens_d_paired(x1, x2)
